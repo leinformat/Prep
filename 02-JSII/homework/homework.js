1ea86234c00d5406280a25daa2dd3b116545a81f
Leonardoinfo <leonardo.moralesqq@gmail.com>
2021-10-29 14:02:33 -0500

    first commit

--- 02-JSII/homework/homework.js
+++ 02-JSII/homework/homework.js
@@ -16,7 +16,7 @@ function incrementarPorUno(array) {
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
-  // Tu código:
+  'hola'
 
 }
 
